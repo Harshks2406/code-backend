238d780f58193a54ef56d99df4f6434503e61f88
Harshks2406 <harshsingh.hks@gmail.com>
2024-08-13 20:20:32 +0530

    Controller and Router Created

--- src/app.js
+++ src/app.js
@@ -19,5 +19,10 @@ app.use(express.urlencoded({
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// Routes import
+import userRouter from "./routes/user.routes.js"
+
+// Routes declaration
+app.use("/api/v1/users", userRouter)
 
 export { app }
